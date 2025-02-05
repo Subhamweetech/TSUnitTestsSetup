3ad31969099caef0e1f3f6029538a9ffaf1697bf
DESKTOP-Subham <subham@weetechsolution.com>
2025-02-05 14:27:29 +0530

    added dependency

---
+++ HelloJest/src/Calculator.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class calculator {
+    static Sum(a, b) {
+        let c = a + b;
+        return c;
+    }
+    static Difference(a, b) {
+        let c = a - b;
+        return c;
+    }
+    static Multiply(a, b) {
+        let c = a * b;
+        return c;
+    }
+}
+exports.default = calculator;
+//# sourceMappingURL=Calculator.js.map
---
+++ HelloJest/tests/Calculator.test.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Calculator_1 = __importDefault(require("../src/Calculator"));
+describe('calculate', function () {
+    it('add', function () {
+        let result = Calculator_1.default.Sum(5, 2);
+        expect(result).toBe(7);
+    });
+    it('substract', function () {
+        let result = Calculator_1.default.Difference(5, 2);
+        expect(result).toBe(3);
+    });
+    it('multiply', function () {
+        let res = Calculator_1.default.Multiply(2, 3);
+        expect(res).toBe(6);
+    });
+});
+//# sourceMappingURL=Calculator.test.js.map
